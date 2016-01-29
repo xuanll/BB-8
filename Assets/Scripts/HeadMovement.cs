f8c9d0906d3e54c21c04415235f779c849c996e7
xuanll <xuanll@yeah.net>
2016-01-29 13:20:36 +0800

    update for Unity 5.1.4f

---
+++ Assets/Scripts/HeadMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _bodyTransform;
+    private Transform _headTransform;
+    [SerializeField]
+    private float distanceFromBody = 0.65f;
+    private SpriteRenderer _headSprite;
+    private float _lastPosX;
+
+	//add head Sprites
+	[SerializeField]
+	private Sprite headLeft;
+	[SerializeField]
+	private Sprite headRight;
+
+    void Start()
+    {
+        _headTransform = GetComponent<Transform>();
+        _headSprite = GetComponent<SpriteRenderer>();
+        _lastPosX = _bodyTransform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        _headTransform.position = Vector2.Lerp(_headTransform.position, new Vector2(_bodyTransform.position.x, _bodyTransform.position.y + distanceFromBody), 0.35f);
+        _headTransform.rotation = Quaternion.Lerp(_headTransform.rotation, Quaternion.Euler(0, 0, 0), 0.35f);
+        if ((_bodyTransform.position.x - _lastPosX) > 0.01f)
+        {
+			//Sprite Render flip not support in Unity5.1.4f Personal
+			//use multiple Sprite image instead
+//            _headSprite.flipX = true;
+			_headSprite.sprite = headRight;
+        }
+        else if ((_bodyTransform.position.x - _lastPosX) < -0.01f)
+        {
+//            _headSprite.flipX = false;
+			_headSprite.sprite = headLeft;
+        }
+        _lastPosX = _bodyTransform.position.x;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+    private Rigidbody2D rBody;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rBody.velocity = new Vector2(rBody.velocity.x, jumpHeight);
+        }
+
+        rBody.velocity = new Vector2(moveSpeed* Input.GetAxis("Horizontal"), rBody.velocity.y);
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    rBody.velocity = new Vector2(moveSpeed, rBody.velocity.y);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    rBody.velocity = new Vector2(-moveSpeed, rBody.velocity.y);
+        //}
+    }
+}
